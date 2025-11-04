88e51aae3caa7f50983bff148b77ebd81ca3fb0f
aliek57 <keilaprado@alunos.utfpr.edu.br>
2025-11-04 09:48:22 -0300

    RM-6: Nova funcionalidade - módulo clientes

--- sistema-bancario-clientes/src/main/java/br/edu/utfpr/td/tsi/sistema/bancario/clientes/regras/ClienteSerivce.java
+++ sistema-bancario-clientes/src/main/java/br/edu/utfpr/td/tsi/sistema/bancario/clientes/regras/ClienteSerivce.java
@@ -3,4 +3,6 @@ package br.edu.utfpr.td.tsi.sistema.bancario.clientes.regras;
 public interface ClienteSerivce {
 	// RM-1: Correção de bug na validação do CPF
 	// RM-4: Correção de cadastro 
+	// RM-6: Nova funcionalidade de verificação de cliente
+	boolean ehClienteEspecial();
 }
